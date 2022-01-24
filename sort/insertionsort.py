c81425cfe577631cd44956f23058c6334dfd7954
LeoRP-12 <leonardorifiol.p@gmail.com>
2022-01-24 16:10:29 -0300

    Treating exceptions and adding typing

--- sort/insertionsort.py
+++ sort/insertionsort.py
@@ -1,19 +1,25 @@
-def sortNumbers(toSort):
-    for number in range(1,len(toSort)):
-        current_number= toSort[number]
-        i=number -1
+from typing import List
 
-        while i>= 0 and current_number < toSort[i]:
-            toSort[i+1] = toSort[i]
-            i-= 1
-        toSort[i+1] = current_number
+def sortNumbers(toSort: List, mode='n') -> List : 
+    try:
+        assert mode=='n' or mode=='r' 
+        if mode=="n":
+            for number in range(1,len(toSort)):
+                current_number= toSort[number]
+                i=number -1
 
-def ReversesortNumbers(toSort):
-    for number in range(1,len(toSort)):
-        current_number= toSort[number]
-        i=number -1
+                while i>= 0 and current_number < toSort[i]:
+                    toSort[i+1] = toSort[i]
+                    i-= 1
+                toSort[i+1] = current_number
 
-        while i>= 0 and current_number > toSort[i]:
-            toSort[i+1] = toSort[i]
-            i-= 1
-        toSort[i+1] = current_number
+        if mode =='r':
+            for number in range(1,len(toSort)):
+                current_number= toSort[number]
+                i=number -1
+                while i>= 0 and current_number > toSort[i]:
+                    toSort[i+1] = toSort[i]
+                    i-= 1
+                toSort[i+1] = current_number
+    except AssertionError:
+        print("allowed values for mode are 'r' (reverse mode) or 'n' (normal mode)")
